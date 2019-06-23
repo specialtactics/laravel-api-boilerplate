30a718dadca489c070f35215fa1c03638ce048ae
Max <contact@maxsnow.me>
2019-06-23 16:58:36 +1000

    Move before function to base repo, to make it more clear to users what is happening with policies

--- app/Policies/BasePolicy.php
+++ app/Policies/BasePolicy.php
@@ -2,8 +2,22 @@
 
 namespace App\Policies;
 
+use App\Models\User;
 use Specialtactics\L5Api\Policies\RestfulModelPolicy;
 
 class BasePolicy extends RestfulModelPolicy
 {
+    /**
+     * Process 'global' authorisation rules
+     *
+     * @param $user
+     * @param $ability
+     * @return bool
+     */
+    public function before(User $user, $ability)
+    {
+        if ($user->isAdmin()) {
+            return true;
+        }
+    }
 }
