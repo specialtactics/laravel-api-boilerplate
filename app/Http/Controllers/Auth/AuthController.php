34c8f8f8ebc65a43c95d00f14a5dc9341a0093e2
Max <contact@maxsnow.me>
2019-10-13 23:04:34 +1100

    More rearranging of use imports according to style

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Illuminate\Http\Request;
 use Specialtactics\L5Api\Http\Controllers\Features\JWTAuthenticationTrait;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class AuthController extends Controller
 {
