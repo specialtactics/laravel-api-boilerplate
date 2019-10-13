14484e0ff4757602cff762c88ee925fd7ba06726
Max <contact@maxsnow.me>
2019-10-13 23:08:30 +1100

    Didn't catch this one (web route prefix)

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,8 +39,6 @@ class RouteServiceProvider extends ServiceProvider
         $this->mapApiRoutes();
 
         //$this->mapWebRoutes();
-
-        //
     }
 
     /**
@@ -52,7 +50,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapWebRoutes()
     {
-        Route::middleware('web')
+        Route::prefix('web')
+             ->middleware('web')
              ->namespace($this->namespace)
              ->group(base_path('routes/web.php'));
     }
