629cdcbb1259d709e728a01ca1ad0b26dd5f5ecc
Max <contact@maxsnow.me>
2020-05-10 17:43:47 +1000

    Various package updates, exception handling update for laravel 7

--- app/Exceptions/ApiExceptionHandler.php
+++ app/Exceptions/ApiExceptionHandler.php
@@ -7,6 +7,8 @@ use Specialtactics\L5Api\Exceptions\RestfulApiExceptionHandler;
 /**
  * This class extends the Dingo API Exception Handler, and can be used to modify it's functionality, if required
  *
+ * Unless you have a specific-use case and understand what you are doing, it's best not to modify this.
+ *
  * Class ApiHandler
  */
 class ApiExceptionHandler extends RestfulApiExceptionHandler
