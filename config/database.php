00a77ddc33c6202833a54408c90510674f2c0b9f
Max <contact@maxsnow.me>
2020-11-24 00:40:52 +1100

    Fix local testing, fix test class name

--- config/database.php
+++ config/database.php
@@ -91,6 +91,19 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'test' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE_TEST', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer'
+        ]
     ],
 
     /*
