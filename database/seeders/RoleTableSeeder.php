b5ab44e7e24c94ba6dffd5a32b98727996d59617
Max <contact@maxsnow.me>
2021-04-30 23:34:21 +1000

    Seeders and some configs

--- database/seeders/RoleTableSeeder.php
+++ database/seeders/RoleTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use App\Models\Role;
 
 class RoleTableSeeder extends BaseSeeder
--- database/seeders/UserStorySeeder.php
+++ database/seeders/UserStorySeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use App\Models\Role;
 use App\Models\User;
 
