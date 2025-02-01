5a55bce206e92a309084d0a0ef5dad89b123a80b
Max Snow <max@covergenius.com>
2025-02-01 15:54:34 +0100

    Add schedule Facade to console

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
