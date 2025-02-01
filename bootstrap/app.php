05b150e4ebfcf7c3b8c4dd383ee3699c7f220b0f
Max Snow <max@covergenius.com>
2025-02-01 15:47:01 +0100

    Move http kernel stuff to app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,11 +7,24 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // API middleware group
+        $middleware->group('api', [
+            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            // \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Specialtactics\L5Api\Http\Middleware\SnakeCaseInputParameterKeys::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ]);
+
+        // Aliases
+        $middleware->alias([
+            'snake_case' => \Specialtactics\L5Api\Http\Middleware\SnakeCaseInputParameterKeys::class,
+            'check_role' => \Specialtactics\L5Api\Http\Middleware\CheckUserRole::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
